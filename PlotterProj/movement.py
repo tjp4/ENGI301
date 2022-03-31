240535d5b0bcf625bb48bc92120788379bfde58d
Demo User <debian@beaglebone.localdomain>
2022-03-31 20:05:26 +0000

    code tested and works, stepper motors too weak / too high step size

--- PlotterProj/movement.py
+++ PlotterProj/movement.py
@@ -1,23 +1,23 @@
 import kinematics
-#import stepperMove
+import stepperMove
+import time
+import math
 
 
 gCode = []
-with open("C:/Users/pitts/Documents/GitHub/ENGI301/PlotterProj/gcodeTest.txt") as f:
+with open("gcodeTest.txt") as f:
     gCode = f.readlines()
 
 #------------------------------adddddd ZERO THING FROM STEPPER MOVE
-xPos = 0
-yPos = 0
+stepperMove.zero()
+
+xPrev = 100
+yPrev = 100
 zPos = 0
 th1 = 0
 th2 = 0
 
 for G in gCode:             #all units always mm, IGNORE G21, ONLY CARE MOVEMENT G0#
-    xPrev = xPos
-    yPrev = yPos
-    zPrev = zPos
-
     if (G[0] == 'G' and G[1] == '0'):
         G = G.replace(" ","")
         moveType = G[2]
@@ -26,29 +26,48 @@ for G in gCode:             #all units always mm, IGNORE G21, ONLY CARE MOVEMENT
         zInd = G.find('Z')
         fInd = G.find('F')
         iInd = G.find('I')
-        
-        end = max(fInd,iInd)
+        if (fInd==-1):
+            fInd = 1000
+        if (iInd==-1):
+            iInd=1000
+        end = min(fInd,iInd)
 
         xPos = xPrev
         yPos = yPrev
-        zPos = zPrev
         if (xInd>0):
-            xPos = G[(xInd+1):(yInd)]
-            yPos = G[(yInd+1):(zInd)]
+            xPos = float(G[(xInd+1):(yInd)])+100
+            yPos = float(G[(yInd+1):(zInd)])+100
         
         if (zInd>0):
-            zPos = G[(zInd+1):end]
+            zPos = float(G[(zInd+1):end])
         
         #------------------ NOW HAVE xPos yPos zPos and moveType
         #move type can be 0,1,2,3 FOR NOW IGNORING 123, ALL linear interp
-        if (zPos<0):
-            #add servo mvmnt
-            print("servo down")
+        
+        pen(zPos) #servo up/down
         
         #add movetype differentation
-        thDes = kinematics.ik(xPos,yPos)
-        #m1(curr,desireed,somespd)
-        th1 = kinematics.deg2step(thDes[0])*1.8
-        th2 = kinematics.deg2step(thDes[1])*1.8
-        
-
+        if (moveType==0):
+            thDes = kinematics.ik(xPos,yPos)
+            stepperMove.m1(th1,thDes[0],1)
+            stepperMove.m2(th2,thDes[1],.5)
+            th1 = kinematics.deg2step(thDes[0])*1.8
+            th2 = kinematics.deg2step(thDes[1])*1.8
+            time.sleep(.2)
+        else:
+            xDist = xPos-xPrev
+            yDist = yPos-yPrev
+            n = round(math.sqrt(xDist**2+yDist**2))
+            for j in range(0,n):
+                xPos = xDist*j/n+xPrev
+                yPos = yDist*j/n+yPrev
+                print(xPos,yPos)
+                thDes = kinematics.ik(xPos,yPos)
+                stepperMove.m1(th1,thDes[0],1)
+                stepperMove.m2(th2,thDes[1],.5)
+                th1 = kinematics.deg2step(thDes[0])*1.8
+                th2 = kinematics.deg2step(thDes[1])*1.8
+                time.sleep(.2)
+        xPrev = xPos
+        yPrev = yPos
+        zPrev = zPos
--- PlotterProj/stepperMove.py
+++ PlotterProj/stepperMove.py
@@ -2,13 +2,22 @@
 import time
 import board
 import digitalio
+import pwmio
 from adafruit_motor import stepper
 import kinematics
+
+import Adafruit_BBIO.PWM as PWM
+
+servo_pin = "P1_36"
+duty_min = 5
+duty_max = 10
+duty_span = duty_max - duty_min
+
+PWM.start(servo_pin, (100-duty_min), 50.0)
 
 #200 steps = 360DEG
 #1.8 degrees per step
 #1 RPM = 360 deg/min = 60 deg/sec = 33.333 step/sec
-
 coils = (
     digitalio.DigitalInOut(board.P2_2),  # A1
     digitalio.DigitalInOut(board.P2_4),  # A2
@@ -35,15 +44,40 @@ def zero():
 
 def m1(curr,new,spd):
     th = new - curr
-    STEPS = kinematics.deg2step(th)
+    STEPS = abs(kinematics.deg2step(th))
     DELAY = 1/(33.3*spd)    #spd in rpm
-
-    if (th>0):
+    if (th<0):
         for step in range(STEPS):
             motor1.onestep()
             time.sleep(DELAY)
-    elif (th<0):
+    elif (th>0):
         for step in range(STEPS):
             motor1.onestep(direction=stepper.BACKWARD)
             time.sleep(DELAY)
 
+def m2(curr,new,spd):
+    th = new - curr
+    STEPS = abs(kinematics.deg2step(th))
+    DELAY = 1/(33.3*spd)    #spd in rpm
+
+    if (th<0):
+        for step in range(STEPS):
+            motor2.onestep()
+            time.sleep(DELAY)
+    elif (th>0):
+        for step in range(STEPS):
+            motor2.onestep(direction=stepper.BACKWARD)
+            time.sleep(DELAY)
+
+def pen(a):
+    if(a>0):
+        angle_f = float(0)
+        duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
+        print(duty)
+        PWM.set_duty_cycle(servo_pin, duty)
+    else:
+        angle_f = float(90)
+        duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
+        print(duty)
+        PWM.set_duty_cycle(servo_pin, duty)
+        
